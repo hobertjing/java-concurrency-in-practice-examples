9e495947d35ccec7d98016dcf689b7b1337bdd12
hobertjing <hongbo.jing@gmail.com>
2016-07-30 23:25:51 +0800

    chapter08

---
+++ src/net/jcip/examples/chapter08/ThreadDeadlock.java
@@ -0,0 +1,42 @@
+package net.jcip.examples.chapter08;
+
+import java.util.concurrent.*;
+
+/**
+ * ThreadDeadlock
+ * <p/>
+ * Task that deadlocks in a single-threaded Executor
+ * 在单线程Executor中任务发生死锁
+ */
+public class ThreadDeadlock {
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    public class LoadFileTask implements Callable<String> {
+        private final String fileName;
+
+        public LoadFileTask(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public String call() throws Exception {
+            // Here's where we would actually read the file
+            return "";
+        }
+    }
+
+    public class RenderPageTask implements Callable<String> {
+        public String call() throws Exception {
+            Future<String> header, footer;
+            header = exec.submit(new LoadFileTask("header.html"));
+            footer = exec.submit(new LoadFileTask("footer.html"));
+            String page = renderBody();
+            // Will deadlock -- task waiting for result of subtask
+            return header.get() + page + footer.get();
+        }
+
+        private String renderBody() {
+            // Here's where we would actually render the page
+            return "";
+        }
+    }
+}
